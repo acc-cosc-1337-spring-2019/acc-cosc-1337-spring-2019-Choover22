12cd2a87f9372c28ac9229f6b14ca36ed97bf255
Connor H <connor.hoovestol@g.austincc.edu>
2019-02-26 16:53:12 -0600

    update 2/26 prior to class

--- homework/03_iteration/dna.cpp
+++ homework/03_iteration/dna.cpp
@@ -1,3 +1,6 @@
+#include "dna.h"
+#include <string>
+#include <iostream>
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -5,13 +8,30 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string & dna)
+{
+	int count = 0;
+	for (int i = 0; i < dna.size(); i++)
+	{
+		if (dna[i] == 'G' || dna[i] == 'C')
+			count++;
+	}
+
+	return count/(dna.size() * 1.0);
+}
 
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+string get_reverse_string(string dna) 
+{
+	for (int i = dna.size() - 1; i >= 0; i--)
+	{
+		temp += dna[i];
+	}
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -23,3 +43,38 @@ b. iterate local string variable and
 c. return string
 
 */
+
+string get_dna_complement(string dna)
+{
+	get_reverse_string(dna);
+	str1 = reverse_string(dna);
+	if (dna[i] == 'A') 
+	{
+		dna[i] == 'T';
+	}
+		
+	else {
+		if (dna[i] == 'T')
+		{
+			dna[i] == 'A';
+		}
+	}
+	else {
+		if (dna[i] == 'C')
+			{
+				dna[i] == 'G';
+			}
+		}
+	else {
+
+
+		if (dna[i] == 'G')
+			{
+				dna[i] == 'C';
+			}
+		}
+	return dna;
+}
+
+
+
--- homework/03_iteration/main.cpp
+++ homework/03_iteration/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using std::cout;
+using std::cin;
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
 or 2 for Get DNA Complement.  The program will prompt user for a 
@@ -7,5 +10,23 @@ user enters a y or Y.
 */
 int main() 
 {
+	menu = 0;
+	cout << "Enter 1 for Get GC Content or 2 for Get DNA complement";
+	cin >> menu;
+	if (menu == 1)
+	{
+		get_gc_content(dna);
+	}
+	else
+		if (menu == 2)
+	{
+			get_dna_complement(dna);
+	}
+	cout << "Do you wish to continue?";
+	cin >> menu;
+	if (menu == 'Y' || menu == 'y')
+	{
+
+	}
 	return 0;
 }
